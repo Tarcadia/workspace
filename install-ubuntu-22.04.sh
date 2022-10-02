72e319449057cb749deccf3024aac0dd836b5fe5
Tarcadia <tarcadia@qq.com>
2022-10-02 20:47:07 +0000

    :sparkles: install: add completion to file.

--- install-ubuntu-22.04.sh
+++ install-ubuntu-22.04.sh
@@ -6,6 +6,7 @@ sudo chsh -s $(which zsh)
 sudo apt install screenfetch lolcat figlet cowsay fortune-mod sl
 
 mkdir ~/.zsh
+cp $PSD/zsh/oh-my-zsh/lib/completion.zsh ~/.zsh/completion.zsh
 cp $PSD/zsh/oh-my-zsh/lib/git.zsh ~/.zsh/git.zsh
 cp $PSD/zsh/theme/tarcadia.zsh-theme ~/.zsh/tarcadia.zsh-theme
 cp $PSD/zsh/zshrc ~/.zshrc
