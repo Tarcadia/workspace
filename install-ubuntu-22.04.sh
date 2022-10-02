30ee24f20d128a89358549f6d9d0ef7c38661945
Tarcadia <tarcadia@qq.com>
2022-10-02 17:36:01 +0000

    :tada: add initial files

---
+++ install-ubuntu-22.04.sh
@@ -0,0 +1,13 @@
+PSD=$(pwd)"/"$(dirname $0)
+
+sudo apt install zsh command-not-found
+sudo chsh -s $(which zsh)
+
+sudo apt install screenfetch lolcat figlet cowsay fortune-mod sl
+
+mkdir ~/.zsh
+cp $PSD/zsh/oh-my-zsh/lib/git.zsh ~/.zsh/git.zsh
+cp $PSD/zsh/oh-my-zsh/lib/theme-and-appearance.zsh ~/.zsh/theme-and-appearance.zsh
+cp $PSD/zsh/theme/tarcadia.zsh-theme ~/.zsh/tarcadia.zsh-theme
+cp $PSD/zsh/zshrc ~/.zshrc
+
