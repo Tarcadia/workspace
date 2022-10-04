cd08620820f0aa9eec27506a70e7c5918e254944
Tarcadia <tarcadia@qq.com>
2022-10-04 21:13:42 +0000

    :sparkles: install: support frp install.

--- install-ubuntu-22.04.sh
+++ install-ubuntu-22.04.sh
@@ -47,3 +47,34 @@ sudo chmod a-x /etc/update-motd.d/80-livepatch
 echo " - Adding MOTD."
 sudo cp $PSD/motd/motd /etc/update-motd.d/50-screenfetch-sysinfo
 sudo chmod a+x /etc/update-motd.d/50-screenfetch-sysinfo
+
+# install node server apps
+echo "Installing FRP."
+echo " - Downloading FRP."
+VERSION="0.44.0"
+if [ $(uname -p) == "x86_64" ]; then
+	curl -o /tmp/frp.tar.gz -J https://github.com/fatedier/frp/releases/download/v${VERSION}/frp_${VERSION}_linux_amd64.tar.gz
+elif [ $(uname -p) == "i386" ]; then
+	curl -o /tmp/frp.tar.gz -J https://github.com/fatedier/frp/releases/download/v${VERSION}/frp_${VERSION}_linux_386.tar.gz
+elif [ $(uname -p) == "i686" ]; then
+	curl -o /tmp/frp.tar.gz -J https://github.com/fatedier/frp/releases/download/v${VERSION}/frp_${VERSION}_linux_386.tar.gz
+elif [ $(uname -p) == "aarch64" ]; then
+	curl -o /tmp/frp.tar.gz -J https://github.com/fatedier/frp/releases/download/v${VERSION}/frp_${VERSION}_linux_arm64.tar.gz
+fi
+if [ -e $HOME/FRP-${VERSION} ]; else
+	mkdir $HOME/FRP-${VERSION}
+fi
+tar -zxvf /tmp/frp.tar.gz -C $HOME/FRP-${VERSION}/
+
+echo " - Bin-ing FRP."
+sudo cp $HOME/FRP-${VERSION}/frps /usr/bin/frps-${VERSION}
+sudo cp $HOME/FRP-${VERSION}/frpc /usr/bin/frpc-${VERSION}
+sudo ln /usr/bin/frps-${VERSION} /usr/bin/frps
+sudo ln /usr/bin/frpc-${VERSION} /usr/bin/frpc
+sudo mkdir /etc/frp
+
+echo " - Systemd-ing FRP."
+sudo cp $PSD/node-server/frps.ini /etc/frp/
+sudo cp $PSD/node-server/frps.service /usr/lib/systemd/system/
+sudo systemctl start frps.service
+sudo systemctl enable frps.service
