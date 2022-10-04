f274ee5ce492a755d06f61303bee9aad79155489
Tarcadia <tarcadia@qq.com>
2022-10-04 19:39:18 +0000

    :zap: install: add output make it more noisy lol.

--- install-ubuntu-22.04.sh
+++ install-ubuntu-22.04.sh
@@ -3,7 +3,12 @@ if ! [ -d $PSD/bk ]; then
 	mkdir $PSD/bk
 fi
 
+# install preferred softs.
+echo "Installing cowsay, firtune-mod, sl."
 sudo apt-get -qq install cowsay fortune-mod sl > /dev/null
+
+# install zsh and theme
+echo "Installing zsh, theme and supports."
 sudo apt-get -qq install zsh command-not-found > /dev/null
 sudo chsh -s $(which zsh)
 
@@ -17,19 +22,27 @@ if [ -e ~/.zsh ]; then
 	echo " - Backing up zshrc."
 	cp -r ~/.zshrc $PSD/bk/
 fi
+
+echo " - Installing theme."
 sudo apt-get -qq install lolcat figlet > /dev/null
 cp $PSD/zsh/oh-my-zsh/lib/completion.zsh ~/.zsh/completion.zsh
 cp $PSD/zsh/oh-my-zsh/lib/git.zsh ~/.zsh/git.zsh
 cp $PSD/zsh/theme/tarcadia.zsh-theme ~/.zsh/tarcadia.zsh-theme
 cp $PSD/zsh/zshrc ~/.zshrc
 
+# install gitconfig
+echo "Installing gitconfig."
 cp $PSD/git/gitconfig ~/.gitconfig
 cp $PSD/screen/screenrc ~/.screenrc
 
+# install MOTD
 sudo apt-get -qq install screenfetch > /dev/null
+echo "Installing MOTD."
+echo " - Disabling unexpected MOTD."
 sudo chmod a-x /etc/update-motd.d/10-help-text
 sudo chmod a-x /etc/update-motd.d/50-motd-news
 sudo chmod a-x /etc/update-motd.d/50-landscape-sysinfo
 sudo chmod a-x /etc/update-motd.d/80-livepatch
+echo " - Adding MOTD."
 sudo cp $PSD/motd/motd /etc/update-motd.d/50-screenfetch-sysinfo
 sudo chmod a+x /etc/update-motd.d/50-screenfetch-sysinfo
